5a749a6e01eed2d234a1580aec201c8abb419ac6
Thành Đỗ Long <thanh.dolong@gmail.com>
2020-01-11 20:59:10 +0100

    Init MVVM+C architecture

---
+++ MatchingGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MatchingGame
+//
+//  Created by Thành Đỗ Long on 11/01/2020.
+//  Copyright © 2020 Thành Đỗ Long. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
